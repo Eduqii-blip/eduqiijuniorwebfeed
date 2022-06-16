8fa676bc4e2cca130cc5796e5c97556cf1e65bd4
Dwamena Bernard Hakeem <64783115+DwamenaHakim@users.noreply.github.com>
2022-06-16 17:49:16 +0000

    refactor

---
+++ allsubjects/courses/careertech/Pretech/pretechpastquestions/years/pretech.js
@@ -0,0 +1,10 @@
+openWindowTwentyEleven = e => {
+        return window.location.assign('2020/index.html')
+      }
+openWindowTwentyTwelve = e => {
+        return window.location.assign('2019/index.html')
+      }
+openWindowTwentyThirteen = e => {
+        return window.location.assign('2018/index.html')
+      }
+      
