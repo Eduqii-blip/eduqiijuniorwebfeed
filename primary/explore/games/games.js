055d48cfc1e6aea260c7a2f9feba147b399fa126
Dwamena Bernard Hakeem <64783115+DwamenaHakim@users.noreply.github.com>
2022-06-17 05:14:51 +0000

    refactor

---
+++ primary/explore/games/games.js
@@ -0,0 +1,18 @@
+// math bits
+mathbits = e => {
+       return window.open('mathbits/','_blank')
+}
+// Word beater
+ wordbeater = e => {
+        return window.open('wordbeater-master/','_blank')
+      }
+ 
+ //word puzzle
+ wordpuzzle = e => {
+       return window.open('wordpuzzle/','_blank')
+ }
+
+  //Eduqii boy
+ eduqiiboy = e => {
+       return window.open('eduqii_boy/','_blank')
+ }
